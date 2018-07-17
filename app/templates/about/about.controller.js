1dab542b01df3e72778c5051620a31f738aef87a
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-07-18 01:07:43 +0300

    add responsive to homepage, add about page with responsive

--- app/templates/about/about.controller.js
+++ app/templates/about/about.controller.js
@@ -11,22 +11,22 @@
         var vm = this;
         vm.values = [
             {
-                image:'content/img/about/1 (1).svg',
+                image:'content/img/about/1 (1).svg.png',
                 title:'about_map_value_item1_title',
                 text:'about_map_value_item1_subtitle'
             },
             {
-                image:'content/img/about/2 (1).svg',
+                image:'content/img/about/2 (1).svg.png',
                 title:'about_map_value_item2_title',
                 text:'about_map_value_item2_subtitle'
             },
             {
-                image:'content/img/about/3 (1).svg',
+                image:'content/img/about/3 (1).svg.png',
                 title:'about_map_value_item3_title',
                 text:'about_map_value_item3_subtitle'
             },
             {
-                image:'content/img/about/4 (1).svg',
+                image:'content/img/about/4 (1).svg.png',
                 title:'about_map_value_item4_title',
                 text:'about_map_value_item4_subtitle'
             }
